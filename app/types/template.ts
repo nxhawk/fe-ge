e078c52679f6bcec6149258188d3345ca317cd60
21120447 <haonhat2729@gmail.com>
2025-07-21 22:43:02 +0700

    feat: select template

--- app/types/template.ts
+++ app/types/template.ts
@@ -1,8 +1,35 @@
-export interface Template {
+export interface TemplateFeature {
+  id: string;
   name: string;
-  label: string;
   description: string;
-  githubRepo: string;
-  tags?: string[];
-  icon?: string;
+  videoUrl?: string;
+  isDefault: boolean;
+}
+
+export interface Template {
+  id: string;
+  name: string;
+  branch: string;
+  backgroundImage: string;
+  githubUrl: string;
+  features: TemplateFeature[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface Feature {
+  id: string;
+  name: string;
+  description: string;
+  videoUrl: string;
+  selected: boolean;
+  isDefault?: boolean;
+}
+
+export interface Framework {
+  id: string;
+  name: string;
+  description: string;
+  logo: string;
+  selected: boolean;
 }
