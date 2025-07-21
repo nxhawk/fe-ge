c78d339f7ef581973f8361a603a0c847959064b6
21120447 <haonhat2729@gmail.com>
2025-07-21 20:49:34 +0700

    chore: update title

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -338,12 +338,12 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
         <div className="flex flex-col lg:flex-row overflow-y-auto w-full h-full">
           <div className={classNames(styles.Chat, 'flex flex-col flex-grow lg:min-w-[var(--chat-min-width)] h-full')}>
             {!chatStarted && (
-              <div id="intro" className="mt-[16vh] max-w-chat mx-auto text-center px-4 lg:px-0">
+              <div id="intro" className="mt-[16vh] max-w-4xl mx-auto text-center px-4 lg:px-0">
                 <h1 className="text-3xl lg:text-6xl font-bold text-bolt-elements-textPrimary mb-4 animate-fade-in">
-                  Where ideas begin
+                  What do you want to <span className="text-purple-600">build</span>?
                 </h1>
                 <p className="text-md lg:text-xl mb-8 text-bolt-elements-textSecondary animate-fade-in animation-delay-200">
-                  Bring ideas to life in seconds or get help on existing projects.
+                  Design, generate, and deploy front-end code for clothing e-commerce websites.
                 </p>
               </div>
             )}
