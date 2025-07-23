a83d2fb1fdb1251da3ba4e4ff5ac86850201a05f
21120447 <haonhat2729@gmail.com>
2025-07-23 22:50:51 +0700

    fix: image template

--- app/components/chat/ChatAlert.tsx
+++ app/components/chat/ChatAlert.tsx
@@ -88,7 +88,7 @@ export default function ChatAlert({ alert, clearAlert, postMessage }: Props) {
                   )}
                 >
                   <div className="i-ph:chat-circle-duotone"></div>
-                  Ask Bolt
+                  Ask AI
                 </button>
                 <button
                   onClick={clearAlert}
